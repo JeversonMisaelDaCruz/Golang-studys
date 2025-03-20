55478c376ef5715e2ec7e995e6b89ef25645acb3
Jeverson misael da cruz filho <jeversonmisaeldacruzfilho@MacBook-Air-de-Jeverson.local>
2025-03-19 22:19:34 -0300

    age for drive vireft in golang

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var person Person
+	fmt.Println("Insert your name:")
+	fmt.Scanln(&person.Name)
+	fmt.Println("Insert your age:")
+	fmt.Scanln(&person.Age)
+	fmt.Println("Your name is", person.Name, "and your age is", person.Age)
+	if person.Age >= 18 {
+		fmt.Println("You can drive")
+	} else {
+		fmt.Println("You can't drive")
+	}
 }
